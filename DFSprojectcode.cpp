1c80b3ccf0e7087f4dc47a7f8c232af6f7aea330
RamyapriyaPutta <90157837+RamyapriyaPutta@users.noreply.github.com>
2021-12-08 19:54:59 -0600

    Update DFSprojectcode.cpp

--- DFSprojectcode.cpp
+++ DFSprojectcode.cpp
@@ -1,145 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-#define MAX 5
+// a given vertex in a given graph
+#include<bits/stdc++.h>
+using namespace std;
 
-struct Vertex {
-   char label;
-   bool visited;
+// Graph class represents a directed graph
+// using adjacency list representation
+class Graph {
+public:
+	map<int, bool> visited;
+	map<int, list<int> > adj;
+
+	// function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// DFS traversal of the vertices
+	// reachable from v
+	void DFS(int v);
 };
 
-//stack variables
-
-int stack[MAX]; 
-int top = -1; 
-
-//graph variables
-
-//array of vertices
-struct Vertex* lstVertices[MAX];
-
-//adjacency matrix
-int adjMatrix[MAX][MAX];
-
-//vertex count
-int vertexCount = 0;
-
-//stack functions
-
-void push(int item) { 
-   stack[++top] = item; 
-} 
-
-int pop() { 
-   return stack[top--]; 
-} 
-
-int peek() {
-   return stack[top];
-}
-
-bool isStackEmpty() {
-   return top == -1;
-}
-
-//graph functions
-
-//add vertex to the vertex list
-void addVertex(char label) {
-   struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
-   vertex->label = label;  
-   vertex->visited = false;     
-   lstVertices[vertexCount++] = vertex;
-}
-
-//add edge to edge array
-void addEdge(int start,int end) {
-   adjMatrix[start][end] = 1;
-   adjMatrix[end][start] = 1;
-}
-
-//display the vertex
-void displayVertex(int vertexIndex) {
-   printf("%c ",lstVertices[vertexIndex]->label);
-}       
-
-//get the adjacent unvisited vertex
-int getAdjUnvisitedVertex(int vertexIndex) {
-   int i;
-
-   for(i = 0; i < vertexCount; i++) {
-      if(adjMatrix[vertexIndex][i] == 1 && lstVertices[i]->visited == false) {
-         return i;
-      }
-   }
-
-   return -1;
-}
-
-void depthFirstSearch() {
-   int i;
-
-   //mark first node as visited
-   lstVertices[0]->visited = true;
-
-   //display the vertex
-   displayVertex(0);   
-
-   //push vertex index in stack
-   push(0);
-
-   while(!isStackEmpty()) {
-      //get the unvisited vertex of vertex which is at top of the stack
-      int unvisitedVertex = getAdjUnvisitedVertex(peek());
-
-      //no adjacent vertex found
-      if(unvisitedVertex == -1) {
-         pop();
-      } else {
-         lstVertices[unvisitedVertex]->visited = true;
-         displayVertex(unvisitedVertex);
-         push(unvisitedVertex);
-      }
-   }
-
-   //stack is empty, search is complete, reset the visited flag        
-   for(i = 0;i < vertexCount;i++) {
-      lstVertices[i]->visited = false;
-   }        
-}
-
-int main() 
+void Graph::addEdge(int v, int w)
 {
-    {
-   int i, j;
+	adj[v].push_back(w); // Add w to v’s list.
+}
 
-   for(i = 0; i < MAX; i++)    // set adjacency 
-      for(j = 0; j < MAX; j++) // matrix to 0
-         adjMatrix[i][j] = 0;
-   }
+void Graph::DFS(int v)
+{
+	// Mark the current node as visited and
+	// print it
+	visited[v] = true;
+	cout << v << " ";
 
-   addVertex('S');   
-   addVertex('A');   
-   addVertex('B');   
-   addVertex('C');   
-   addVertex('D'); 
-   addVertex ('E');   
-   addVertex ('F');
-   addVertex ('G');
+	// Recur for all the vertices adjacent
+	// to this vertex
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFS(*i);
+}
 
-   addEdge (0, 1);    
-   addEdge (0, 2);    
-   addEdge (0, 3);    
-   addEdge(1, 4);    
-   addEdge(5, 2);    
-   addEdge(3, 6);
-   addEdge(4, 7);
-   addEdge(5, 7);
-   addEdge(6, 7);
+// Driver code
+int main()
+{
+	// Create a graph given in the above diagram
+	Graph g;
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(0, 3);
+	g.addEdge(1, 4);
+	g.addEdge(4, 7);
+	g.addEdge(7, 5);
+	g.addEdge(5, 2);
+	g.addEdge(7, 6);
+	g.addEdge(6, 3);
+	
 
-   printf("Depth First Search: ");
-   depthFirstSearch(); 
+	cout << "Following is Depth First Traversal"
+			" (starting from vertex 0) \n";
+	g.DFS(0);
 
-   return 0;   
+	return 0;
 }
